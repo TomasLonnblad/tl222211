e476bcb8e446a756122b5ffc8d5f7cc40da5abc7
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-28 16:25:35 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/background-ripple-effect-demo'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
